c70d6b2246d305dfb39e63041a7bc025357fbc18
Billy Ceskavich <bceskavich@gmail.com>
2015-06-30 14:20:00 -0400

    Initial Flask files.

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+DEBUG = False
+CSRF_ENABLED = True
+SECRET_KEY = 'TODO REPLACE NOW!'
+CSRF_SECRET_KEY = 'TODO SERIOUSLY!!'
+
+VERSION = '0.1.0'
