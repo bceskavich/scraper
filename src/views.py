14423b421aa7ca6ed953c206be3d05b367682868
Billy Ceskavich <bceskavich@gmail.com>
2015-06-30 20:23:56 -0400

    Structure outline for RESTful URL params.

--- src/views.py
+++ src/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, jsonify
 
 from src import app
 
@@ -6,12 +6,26 @@ from src import app
 def index():
     return render_template('index.html')
 
-@app.route('/scrape?token=<string:token>&id=<string:id>', methods=['GET'])
-def scrape(token, id):
-    """ TODO """
+@app.route('/scrape/', methods=['GET'])
+def scrape():
+    if request.method == 'GET':
+        token = request.args.get('token', None)
+        fb_id = request.args.get('id', None)
 
-@app.route('/get_id?token=<string:token>&object=<string:object_name', methods=['GET'])
-def get_id(token, object_name):
+        if token is None or fb_id is None:
+            resp = {
+                'data': None,
+                'status': 0,
+                'message': 'Invalid parameters. Please provide a token and id.'
+            }
+        else:
+            # TODO - actually call the scraper function
+            resp = {'todo': 'TODO'}
+
+    return jsonify(resp)
+
+@app.route('/get_id/<string:object_name>', methods=['GET'])
+def get_id(object_name):
     """ TODO """
 
 """
